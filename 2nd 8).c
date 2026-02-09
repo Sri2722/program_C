3e28ee3b09e0de31fd9f20de8648f0384861cac4
Sri2722 <srinaveena23@gmail.com>
2026-02-09 16:33:01 +0530

    Add files via upload

---
+++ 2nd 8).c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+    int a=2;
+    printf("%d",a*=10);
+    int b=25;
+    printf("\n%d",b*=2);
+    return 0;
+}
---
+++ 2nd 9).c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int a=40;
+    int b=10;
+    printf("%d",a/b);
+    int c=100;
+    int d=20;
+    printf("\n%d",c/d);
+    return 0;
+    return 0;
+    
+}
