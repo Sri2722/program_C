873cb4a26eaeac12ee6c5a88a1522effcf1e2c2c
Sri2722 <srinaveena23@gmail.com>
2026-02-10 12:19:30 +0530

    Add files via upload

---
+++ 3rd 9).c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int a;
+    scanf("%d",&a);
+    float b=a;
+    printf("%0.1f",b);
+    int c;
+    scanf("%d",&c);
+    float d=c;
+    printf("%.0f",d);
+    
+    return 0;
+}
